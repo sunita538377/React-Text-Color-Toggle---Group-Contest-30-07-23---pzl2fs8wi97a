04eeeeadf964e0baa1bde82d76c6525a5e84e14f
Akashlahane <68777702+Akashlahane@users.noreply.github.com>
2023-07-30 18:42:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@ import React, {useEffect, useState} from 'react'
 import styles from '../styles/App.css';
 
 const App = () => {
+  
   const [togglest, settoggle]=useState("false");
+  
   const togglefunc= ()=>{
     settoggle(!togglest);
   };
+  
   return (
     <div id="main">
       <p className={togglest ? styles.blueColor : styles.redColor} >Newton School</p>
